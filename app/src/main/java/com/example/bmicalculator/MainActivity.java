abeebfc11eca0ea601a6624f5df52ab804633eca
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-03-13 16:27:21 +0100

    added references to all necessary xml elements for further modification

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView resultText = findViewById(R.id.text_view_result);
+        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+        Button calculateButton = findViewById(R.id.button_calculate);
+
+
     }
+
+
 }
