7524dd18a1dcfbeb1828087ba17bc3d645f68f6e
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-03-13 22:56:47 +0100

    created nethods for BMIcalculation (TODO) and setting up click listner, parsed user's strings into integers

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.bmicalculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultText;
+    private RadioButton maleRadioButton;
+    private RadioButton femaleRadioButton;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText weightEditText;
+    private Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +29,45 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        TextView resultText = findViewById(R.id.text_view_result);
-        RadioButton maleRadioButton = findViewById(R.id.radio_button_male);
-        RadioButton femaleRadioButton = findViewById(R.id.radio_button_female);
-        EditText ageEditText = findViewById(R.id.edit_text_age);
-        EditText feetEditText = findViewById(R.id.edit_text_feet);
-        EditText inchesEditText = findViewById(R.id.edit_text_inches);
-        EditText weightEditText = findViewById(R.id.edit_text_weight);
-        Button calculateButton = findViewById(R.id.button_calculate);
-
+        findViews();
+        setupButtonClickListener();
 
     }
 
+
+
+    private void findViews() {
+        resultText = findViewById(R.id.text_view_result);
+        maleRadioButton = findViewById(R.id.radio_button_male);
+        femaleRadioButton = findViewById(R.id.radio_button_female);
+        ageEditText = findViewById(R.id.edit_text_age);
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        weightEditText = findViewById(R.id.edit_text_weight);
+        calculateButton = findViewById(R.id.button_calculate);
+    }
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                calculateBmi();
+            }
+        });
+    }
+
+    private void calculateBmi()
+    {
+        String ageText = ageEditText.getText().toString();
+        String feetText = feetEditText.getText().toString();
+        String inchesText= inchesEditText.getText().toString();
+        String weightText  = weightEditText.getText().toString();
+
+        int age = Integer.parseInt(ageText);
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+    }
 
 }
